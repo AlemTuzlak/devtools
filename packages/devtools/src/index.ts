60d18418e780bc4045ac3a74b25b8a9ba37c2d19
Alem Tuzlak <t.zlak@hotmail.com>
2025-07-25 11:21:11 +0200

    fix

--- packages/devtools/src/index.ts
+++ packages/devtools/src/index.ts
@@ -1,5 +1,4 @@
 // This is your packages entry point, everything exported from here will be accessible to the end-user.
-import { Shell as Primitive } from "./client/shell"
+import { Shell } from "./client/shell"
 
-const Shell = typeof window !== "undefined" ? Primitive : () => null
 export { Shell }
